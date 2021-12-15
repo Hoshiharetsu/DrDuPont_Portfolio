ad30577493afbceef6395db2ca01b77f2992a4ed
Rebecca DuPont <RElinich@gmail.com>
2021-12-15 06:14:11 -0500

    Reupload
    Uploading Local to Live

---
+++ assets/css/classOverlays/overlayControls.js
@@ -0,0 +1,21 @@
+//Start for CCAC Controls
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+//Start for IUP Controls
+  function onIUP(){
+    document.getElementById("overlay2").style.display = "block";
+  }
+  function offIUP() {
+    document.getElementById("overlay2").style.display = "none";
+  }
+  //Start for Duquesne Controls
+    function onDuq(){
+      document.getElementById("overlay3").style.display = "block";
+    }
+    function offDuq() {
+      document.getElementById("overlay3").style.display = "none";
+    }
---
+++ assets/js/ShowAndHide.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+
+  $("#flip2").click(function(){
+    $("#panel2").slideToggle("slow");
+  });
+
+});
